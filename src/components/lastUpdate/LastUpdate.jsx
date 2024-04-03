b3a2cc840a463fc4caf13031e38eec308e00a2e8
Daria Artiomenko <daridaraart@gmail.com>
2024-04-03 18:18:12 +0300

    modify lastUpdate

--- src/components/lastUpdate/LastUpdate.jsx
+++ src/components/lastUpdate/LastUpdate.jsx
@@ -1,14 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getExchange } from "../../services/getResponse";
 import circle from '../../assets/img/circle.svg'
 import './lastUpdate.scss'
 
 const LastUpdate = () => {
+    const lastUpdateData = useQuery({ queryKey: ["lastUpdate"], queryFn: getExchange });
+
+    const lastUpdate = lastUpdateData.data?.data.meta.last_updated_at.split('T')[1].slice(0, -1);
+    console.log(lastUpdate);
     return (
         <section className='last-update'> 
             <div className='last-update-container'> 
                 <div className='last-update-block'>
                     <img src={circle} alt="circle"/>
                     <div className='last-update-text'>
-                        Last updated at <span>11:59pm</span>
+                        Last updated at <span>{lastUpdate}</span>
                     </div>
                 </div>
             </div>
