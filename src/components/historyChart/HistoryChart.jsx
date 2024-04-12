a14f03e17f795755c08755afdbf4f27c15e13ee7
Daria Artiomenko <daridaraart@gmail.com>
2024-04-12 10:34:20 +0300

    ended adaptation

--- src/components/historyChart/HistoryChart.jsx
+++ src/components/historyChart/HistoryChart.jsx
@@ -18,52 +18,56 @@ const HistoryChart = () => {
     return (
         <section className="history">
             <div className="history-select">
-                <label htmlFor="" className="history-select-label">
-                    From
-                    <select type="select" className="history-select-curr" value={selectCurrFrom} onChange={(e) => setSelectCurrFrom(e.target.value)}>
-                    {Object.keys(currenciesInfo).map((key) => (
-                        <option 
-                            key={key} 
-                            className="history-select-curr-option"
-                            onChange={(e) => setSelectCurrFrom(e.target.value)}
-                            >
-                            {key}
-                        </option>
-                    ))}
-                    </select>
-                </label>
-                <label htmlFor="" className="history-select-label">
-                    To
-                    <select type="select" className="history-select-curr" value={selectCurrTo} onChange={(e) => setSelectCurrTo(e.target.value)}>
-                    {Object.keys(currenciesInfo).map((key) => (
-                        <option 
-                            key={key} 
-                            className="history-select-curr-option"
-                            onChange={(e) => setSelectCurrTo(e.target.value)}
-                            >
-                            {key}
-                        </option>
-                    ))}
-                    </select>
-                </label>
-
-                <label htmlFor="" className="history-select-label">
-                    from
-                    <input className="history-select-date" 
-                        value={rangeStart} 
-                        type="date" 
-                        min="2022-01-01" 
-                        onChange={(e) => setRangeStart(e.target.value)}/>
-                </label>
-                <label htmlFor="" className="history-select-label">
-                    to
-                    <input className="history-select-date" 
-                        value={rangeEnd}
-                        type="date"
-                        min="2022-01-01"
-                        onChange={(e) => setRangeEnd(e.target.value)}/>
-                </label>
-
+                <div className="history-select-currency">
+                    <label htmlFor="" className="history-select-label">
+                        From
+                        <select type="select" className="history-select-curr" value={selectCurrFrom} onChange={(e) => setSelectCurrFrom(e.target.value)}>
+                        {Object.keys(currenciesInfo).map((key) => (
+                            <option 
+                                key={key} 
+                                className="history-select-curr-option"
+                                onChange={(e) => setSelectCurrFrom(e.target.value)}
+                                >
+                                {key}
+                            </option>
+                        ))}
+                        </select>
+                    </label>
+                    <label htmlFor="" className="history-select-label">
+                        To
+                        <select type="select" className="history-select-curr" value={selectCurrTo} onChange={(e) => setSelectCurrTo(e.target.value)}>
+                        {Object.keys(currenciesInfo).map((key) => (
+                            <option 
+                                key={key} 
+                                className="history-select-curr-option"
+                                onChange={(e) => setSelectCurrTo(e.target.value)}
+                                >
+                                {key}
+                            </option>
+                        ))}
+                        </select>
+                    </label>
+                </div>
+                
+            
+                <div className="history-select-period">
+                    <label htmlFor="" className="history-select-label">
+                        From
+                        <input className="history-select-date" 
+                            value={rangeStart} 
+                            type="date" 
+                            min="2022-01-01" 
+                            onChange={(e) => setRangeStart(e.target.value)}/>
+                    </label>
+                    <label htmlFor="" className="history-select-label">
+                        To
+                        <input className="history-select-date" 
+                            value={rangeEnd}
+                            type="date"
+                            min="2022-01-01"
+                            onChange={(e) => setRangeEnd(e.target.value)}/>
+                    </label>
+                </div>
             </div>
             <div id="history-chart">
                 <canvas id="myChart">
